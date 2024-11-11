13b9809c5907baa2d5edd3db15e349ce28950d5c
veles-rgb <181009546+veles-rgb@users.noreply.github.com>
2024-11-11 14:35:27 -0500

    Add basic math functions & operate function

--- scripts.js
+++ scripts.js
@@ -1,1 +1,34 @@
-// scripts
+let num1 = null;
+let operator = null;
+let num2 = null;
+let displayText = null;
+
+function add(num1, num2) {
+    return num1 + num2;
+};
+
+function subtract(num1, num2) {
+    return num1 - num2;
+};
+
+function multiply(num1, num2) {
+    return num1 * num2;
+};
+
+function divide(num1, num2) {
+    return num1 / num2;
+};
+
+function operate(num1, operator, num2) {
+    if (operator === "+") {
+        return add(num1, num2);
+    } else if ( operator === "-") {
+        return subtract(num1, num2);
+    } else if (operator === "*") {
+        return multiply(num1, num2);
+    } else if (operator === "/") {
+        return divide(num1, num2);
+    } else {
+        return "ERROR (invalid operator)"
+    };
+};
